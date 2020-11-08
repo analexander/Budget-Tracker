99dbfd990ef643cf29b641c3d91eca008e7b9570
Angel Alexander <angale22@gmail.com>
2020-11-08 12:01:16 -0600

    adding onsuccess function to db.js

--- Develop/public/db.js
+++ Develop/public/db.js
@@ -6,8 +6,13 @@ const req = indexedDB.open('budget', 1);
 
 // requests depending on what returns as status
 
+// make objectstore named pending, when permutation added increase number
 req.onupgradeneeded = ({ target }) => {
     let db = target.result;
     db.createObjectStore('pending',  {autoIncrement: true} );
 };
 
+//if db request is successful, take db and refer to that as target
+req.onsuccess = ({ target }) => {
+    db = target.result;
+}
