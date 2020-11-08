9607493497abba2838dbe99d1785653c0c4cdc4c
Angel Alexander <angale22@gmail.com>
2020-11-08 11:58:04 -0600

    adding start of request status functions onupgradeneeded

--- Develop/public/db.js
+++ Develop/public/db.js
@@ -1,3 +1,13 @@
 const indexedDB = window.indexedDB
 
+// instantiate indexedDB, name of collection + version
+
 const req = indexedDB.open('budget', 1);
+
+// requests depending on what returns as status
+
+req.onupgradeneeded = ({ target }) => {
+    let db = target.result;
+    db.createObjectStore('pending',  {autoIncrement: true} );
+};
+
